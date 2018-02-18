43eb710d1bcda54ca6ec48d4a89ac2f4fa0c9cf4
CM Lubinski <cm.lubinski@gmail.com>
2018-02-18 13:09:14 -0500

    Refactor, add --replace flag to load_transmittal.
    Previously, the command would error out when ran multiple times. It's now
    idempotent.

--- mapusaurus/respondents/management/commands/load_transmittal.py
+++ mapusaurus/respondents/management/commands/load_transmittal.py
@@ -1,52 +1,108 @@
 import argparse
 import csv
+import logging
+from typing import BinaryIO, Dict, Iterator, List, TypeVar
 
+from django.core.exceptions import ValidationError
 from django.core.management.base import BaseCommand
+from django.db import transaction
 
 from respondents.models import Institution, Agency
 from respondents.zipcode_utils import create_zipcode
 
+logger = logging.getLogger(__name__)
+
+
+def load_from_csv(agencies: Dict[int, Agency], csv_file: BinaryIO):
+    line_number = 1
+    transmittal_reader = csv.reader(csv_file, delimiter='\t')
+    for line in transmittal_reader:
+        if len(line) != 22:
+            logger.warning("Line %s is invalid, has length %s (expected 22)",
+                           line_number, len(line))
+            break
+        year = line[0]
+        zipcode_city = create_zipcode(
+            city=line[6], state=line[7], year=year, zip_code=line[8])
+
+        agency_id = line[2]
+        if not agency_id.isdigit() or int(agency_id) not in agencies:
+            logger.warning("Line %s has an invalid agency: %s",
+                           line_number, agency_id)
+            break
+
+        assets = line[17]
+        if not assets.isdigit():
+            logger.warning("Line %s has an invalid asset amount: %s",
+                           line_number, assets)
+            break
+
+        respondent_id = line[1]
+
+        inst = Institution(
+            year=year,
+            respondent_id=line[1],
+            agency=agencies[int(agency_id)],
+            institution_id=f"{year}{agency_id}{respondent_id}",
+            tax_id=line[3],
+            name=line[4],
+            mailing_address=line[5],
+            zip_code=zipcode_city,
+            assets=int(assets),
+        )
+        try:
+            inst.clean_fields()
+        except ValidationError:
+            logger.exception('Line %s has invalid data:', line_number)
+            break
+
+        yield inst
+        line_number += 1
+
+
+T = TypeVar('T')
+
+
+def batches(elts: Iterator[T], batch_size:int=100) -> Iterator[List[T]]:
+    """Split an iterator of elements into an iterator of batches."""
+    batch = []
+    for elt in elts:
+        if len(batch) == batch_size:
+            yield batch
+            batch = []
+        batch.append(elt)
+    yield batch
+
+
+def load_save_batches(
+        agencies: Dict[int, Agency], csv_file: BinaryIO, replace: bool=False):
+    """Load Institutions from csv, replace duplicates if desired, then
+    save."""
+    institutions = load_from_csv(agencies, csv_file)
+    for batch in batches(institutions):
+        ids = {inst.institution_id for inst in batch}
+        existing = Institution.objects.filter(institution_id__in=ids)
+        if replace:
+            existing.delete()
+        else:
+            existing_ids = {
+                inst_id for inst_id
+                in existing.values_list('institution_id', flat=True)
+            }
+            batch = [inst for inst in batch
+                     if inst.institution_id not in existing_ids]
+        Institution.objects.bulk_create(batch)
+
 
 class Command(BaseCommand):
-    help = "Loads the data from a HMDA Transmittal Sheet."
+    help = "Loads data from a HMDA Transmittal Sheet."
 
     def add_arguments(self, parser):
         parser.add_argument('file_name', type=argparse.FileType('r'))
+        parser.add_argument('--replace', action='store_true')
 
+    @transaction.atomic
     def handle(self, *args, **options):
         agencies = Agency.objects.get_all_by_code()
-
-        transmittal_reader = csv.reader(options['file_name'], delimiter='\t')
-        institutions = []
-        # count = 1 # use if want to see which item failed, see comment below where we create institution individually
-        for inst_line in transmittal_reader:
-            if len(inst_line) != 22:
-                print(f"Skipping line {inst_line}")
-                break
-            year = inst_line[0]
-            zip_code = inst_line[8]
-            state = inst_line[7]
-            city = inst_line[6]
-            zipcode_city = create_zipcode(zip_code, city, state, year)
-
-            agency = agencies[int(inst_line[2])]
-
-            inst = Institution(
-                year=year,
-                respondent_id=inst_line[1],
-                agency=agency,
-                institution_id=year+inst_line[2]+inst_line[1],
-                tax_id=inst_line[3],
-                name=inst_line[4],
-                mailing_address=inst_line[5],
-                zip_code=zipcode_city,
-                assets=int(inst_line[17]),
-            )
-
-            # This can be used to figure out which exact item was failing, will need to disable bulk create below to use this
-            # Institution.objects.create(inst)
-            # inst.save()
-            # count += 1
-            institutions.append(inst)
-        Institution.objects.bulk_create(institutions)
+        load_save_batches(agencies, options['file_name'], options['replace'])
         options['file_name'].close()
