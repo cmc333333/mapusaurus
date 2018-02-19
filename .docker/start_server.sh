c15fb0ba72aa501a811489692e72b7dbd71157ec
CM Lubinski <cm.lubinski@gmail.com>
2018-02-18 20:08:34 -0500

    Remove directory layer.
    No more `bin/python mapusaurus/manage.py`

--- .docker/start_server.sh
+++ .docker/start_server.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 if [ "${DEBUG^^}" == "TRUE" ]; then
-  python mapusaurus/manage.py runserver 0.0.0.0:"$PORT"
+  python manage.py runserver 0.0.0.0:"$PORT"
 else
-  cd mapusaurus
   gunicorn mapusaurus.wsgi:application -b 0.0.0.0:"$PORT" --access-logfile -
 fi
