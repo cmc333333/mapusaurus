c836aca51af32993228d31e254612f2703935c21
CM Lubinski <cm.lubinski@gmail.com>
2018-03-31 01:48:01 -0400

    Add reports for lender actions.
    How much of each group gets denied or approved? How does it compare to white
    applicants?

--- reports/urls.py
+++ reports/urls.py
@@ -1,10 +1,13 @@
 from django.conf.urls import url
 
+from reports.views.actions import approvals, denials
 from reports.views.applicants import applications, originations
 from reports.views.demographics import demographics
 
 urlpatterns = [
     url(r'^applications/$', applications),
+    url(r'^approvals/$', approvals),
     url(r'^demographics/$', demographics),
+    url(r'^denials/$', denials),
     url(r'^originations/$', originations),
 ]
