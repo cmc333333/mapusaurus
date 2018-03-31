40fa5e52b351fd7e8e7033ead6c3e01e3fbce1aa
CM Lubinski <cm.lubinski@gmail.com>
2018-03-31 01:11:10 -0400

    Add applications/originations reports.
    These reports give us info about the applicants -- what types of applicants
    are requesting loans? What types of applicants receive loans?

--- reports/urls.py
+++ reports/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls import url
 
+from reports.views.applicants import applications, originations
 from reports.views.demographics import demographics
 
 urlpatterns = [
+    url(r'^applications/$', applications),
     url(r'^demographics/$', demographics),
+    url(r'^originations/$', originations),
 ]
