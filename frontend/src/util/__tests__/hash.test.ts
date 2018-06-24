1c0e3d5f4afd50e7212558bc617c86c3f7881266
CM Lubinski <cm.lubinski@gmail.com>
2018-06-24 14:16:11 -0400

    Add HMDA configuration (de)serialization.
    We'll track the lender/location in the URL hash. I don't like all of the
    nested logic around undefined attributes here, but it does give us
    prettier URLs.

--- frontend/src/util/__tests__/hash.test.ts
+++ frontend/src/util/__tests__/hash.test.ts
@@ -1,9 +1,10 @@
 import { Set } from "immutable";
 
+import { LARPointFactory, StoreFactory } from "../../testUtils/Factory";
 import { deserialize, serialize } from "../hash";
 
 describe("deserialize()", () => {
-  it("loads data", () => {
+  it("loads viewport data", () => {
     const result = deserialize(
       "a=param&latitude=11.111&longitude=22&zoom=3&other=stuff",
       { some: "config" },
@@ -19,7 +20,7 @@ describe("deserialize()", () => {
     });
   });
 
-  it("defaults to NaNs", () => {
+  it("defaults viewport to NaNs", () => {
     const result = deserialize("", { some: "config" });
     expect(result).toEqual({
       config: { some: "config" },
@@ -29,6 +30,18 @@ describe("deserialize()", () => {
         zoom: NaN,
       },
       visibleLayers: Set<string>(),
+    });
+  });
+
+  it("loads HMDA data", () => {
+    const result = deserialize("lender=1234&metro=678&county=910", {});
+    expect(result.hmda).toEqual({
+      config: {
+        county: "910",
+        lender: "1234",
+        metro: "678",
+      },
+      lar: [],
     });
   });
 });
@@ -56,5 +69,23 @@ describe("serialize()", () => {
     expect(result).not.toMatch(/config/);
     expect(result).not.toMatch(/aaa/);
     expect(result).not.toMatch(/aToken/);
+  });
+
+  it("serializes hmda config", () => {
+    const result = serialize(StoreFactory.build({
+      hmda: {
+        config: {
+          county: "aaaa",
+          lender: "bbbb",
+          metro: "cccc",
+        },
+        lar: [LARPointFactory.build()],
+      },
+    }));
+
+    expect(result).toMatch(/\bcounty=aaaa\b/);
+    expect(result).toMatch(/\blender=bbbb\b/);
+    expect(result).toMatch(/\bmetro=cccc\b/);
+    expect(result).not.toMatch(/lar/);
   });
 });
