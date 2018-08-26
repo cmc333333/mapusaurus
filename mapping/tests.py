a4d99340eb593e6fe598cd10bf2a0cc7be7f2a52
CM Lubinski <cm.lubinski@gmail.com>
2018-08-26 16:36:44 -0400

    Add state list to SPA.
    This'll be used in the county selector.

--- mapping/tests.py
+++ mapping/tests.py
@@ -206,3 +206,14 @@ def test_spa_contains_years(monkeypatch):
     views.single_page_app(Mock())
     context = views.render.call_args[0][2]
     assert json.loads(context['SPA_CONFIG'])['years'] == [2013, 2011, 2010]
+
+
+@pytest.mark.django_db
+def test_spa_contains_states(monkeypatch):
+    monkeypatch.setattr(views, "render", Mock())
+    views.single_page_app(Mock())
+    context = views.render.call_args[0][2]
+    states = json.loads(context["SPA_CONFIG"])["states"]
+    assert len(states) >= 50
+    assert {"abbr": "IL", "fips": "17", "name": "Illinois"} in states
+    assert {"abbr": "NY", "fips": "36", "name": "New York"} in states
