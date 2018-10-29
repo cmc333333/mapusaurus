1e24690a04568b3f36d9375c0cb952fe8714466d
CM Lubinski <cm.lubinski@gmail.com>
2018-10-28 23:17:00 -0400

    Switch the app root to use the SPA.
    The rewrite's now at a phase where we can replace the previous map and
    delete a bunch of templates and unused views. This is _not_ a full list,
    but it'll get us started.

--- mapusaurus/urls.py
+++ mapusaurus/urls.py
@@ -1,17 +1,17 @@
 from django.conf.urls import include, url
-from django.contrib import admin
 from django.views.generic.base import RedirectView
+
+from mapping.views import single_page_app
 
 
 urlpatterns = [
-    url(r'^$', RedirectView.as_view(url='/institutions/', permanent=False)),
+    url(r'^$', single_page_app),
     url(r'^api/', include('api.urls')),
     url(r'^institutions/', include('respondents.urls',
                                    namespace='respondents')),
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^shapes/', include('geo.urls', namespace='geo')),
     url(r'^hmda/', include('hmda.urls', namespace='hmda')),
-    url(r'^map/', include('mapping.urls')),
+    url(r'^map/spa/$', RedirectView.as_view(url="/", permanent=False)),
     url(r'^census/', include('censusdata.urls', namespace='censusdata')),
     url(r'^reports/', include('reports.urls', namespace='reports')),
 ]
--- respondents/urls.py
+++ respondents/urls.py
@@ -5,12 +5,7 @@ from respondents import views
 
 
 urlpatterns = [
-    url(r'^(?P<year>[0-9]{4})/(?P<agency_id>[0-9])(?P<respondent>[0-9-]{10})/metro/?$',
-        views.select_metro, name='select_metro'),
     url(r'^search/$', views.search_results, name='search_results'),
-    url(r'^$', views.search_home, name='search_home'),
-    url(r'^respondent/(?P<year>[0-9]{4})(?P<agency_id>[0-9])(?P<respondent>[0-9-]{10})',
-        views.respondent, name='respondent_profile'),
     url(r'^branchLocations/(?P<northEastLat>-?\d+\.\d{6})/(?P<northEastLon>-?\d+\.\d{6})/(?P<southWestLat>-?\d+\.\d{6})/(?P<southWestLon>-?\d+\.\d{6})$',
         views.branch_locations_as_json, name='branch_locations'),
 ]
