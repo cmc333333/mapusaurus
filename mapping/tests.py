692e19772c53f5629f6dc0bcb8dba85b391036ae
CM Lubinski <cm.lubinski@gmail.com>
2018-12-26 12:28:29 -0500

    Use yattag for template rendering.
    As we'll be rendering our reports in HTML (prior to converting them to
    PDF), it makes sense to move more template-rendering logic into Python.
    While we could use Django's templates to render the reports, it makes
    more sense to generate the report markup within the same language we're
    using to fetch the data.
    
    This takes a step in that direction by replacing our (one) templated
    endpoint with a version built with the yattag library. The only annoying
    bit here was that, in the short term, Django Analytical continues to
    make sense for generating our analytics tags, so we needed to jump
    through a little hoop to continue to render it.

--- mapping/tests.py
+++ mapping/tests.py
@@ -1,4 +1,3 @@
-import json
 from unittest.mock import Mock
 
 import pytest
@@ -8,23 +7,10 @@ from mapping import views
 
 
 @pytest.mark.django_db
-def test_spa_contains_years(monkeypatch):
-    monkeypatch.setattr(views, "render", Mock())
+def test_spa_contains_years():
     LARYear.objects.create(year=2010)
     LARYear.objects.create(year=2011)
     LARYear.objects.create(year=2013)
 
-    views.single_page_app(Mock())
-    context = views.render.call_args[0][2]
-    assert json.loads(context['SPA_CONFIG'])['years'] == [2013, 2011, 2010]
-
-
-@pytest.mark.django_db
-def test_spa_contains_states(monkeypatch):
-    monkeypatch.setattr(views, "render", Mock())
-    views.single_page_app(Mock())
-    context = views.render.call_args[0][2]
-    states = json.loads(context["SPA_CONFIG"])["states"]
-    assert len(states) >= 50
-    assert {"abbr": "IL", "fips": "17", "name": "Illinois"} in states
-    assert {"abbr": "NY", "fips": "36", "name": "New York"} in states
+    result = views.single_page_app(Mock())
+    assert b"[2013, 2011, 2010]" in result.content
