a8c03b7ba2e999bd132cd38703929fb5cb585dc4
CM Lubinski <cmc333333@gmail.com>
2018-12-28 22:56:41 -0500

    Revert "Use yattag for template rendering."

---
+++ mapusaurus/context_processors.py
@@ -0,0 +1,8 @@
+from django.conf import settings
+
+
+def inject_app_title(request):
+    return {
+        'APP_TITLE': getattr(settings, 'APP_TITLE', ''),
+        'MAPBOX_TOKEN': getattr(settings, 'MAPBOX_TOKEN', ''),
+    }
