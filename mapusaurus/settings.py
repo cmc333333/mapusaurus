f851a7d0ed4623ed97084b8d334c78c0fa7e3d6d
CM Lubinski <cm.lubinski@gmail.com>
2019-01-26 22:39:54 -0500

    Configure Nginx for media files.
    These'll be generated by the worker.

--- mapusaurus/settings.py
+++ mapusaurus/settings.py
@@ -81,6 +81,8 @@ USE_L10N = True
 USE_TZ = True
 
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'out')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [
