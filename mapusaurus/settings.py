5a1863313a7f8b607d78dc8c91b260ca7f0d8ab1
CM Lubinski <cm.lubinski@gmail.com>
2019-02-02 17:02:53 -0500

    Add email configuration.
    We default to Django's defaults if the env vars aren't defined.

--- mapusaurus/settings.py
+++ mapusaurus/settings.py
@@ -1,5 +1,6 @@
 import json
 import os
+from types import MappingProxyType
 
 import dj_database_url
 from django.utils.crypto import get_random_string
@@ -143,3 +144,21 @@ MAPBOX_TOKEN = os.environ.get(
 
 GOOGLE_ANALYTICS_ANONYMIZE_IP = True
 GOOGLE_ANALYTICS_SITE_SPEED = True
+
+if DEBUG:
+    EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "localhost")
+EMAIL_PORT = int(os.environ.get("EMAIL_PORT", "25"))
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+DEFAULT_FROM_EMAIL = os.environ.get(
+    "DEFAULT_FROM_EMAIL", "webmaster@localhost")
+EMAIL_USE_TLS = True
+EMAIL_TIMEOUT = 60
+
+REPORT_EMAIL_KWARGS = MappingProxyType(   # ensures the config is immutable
+    json.loads(os.environ.get(
+        "REPORT_EMAIL_ARGS",
+        json.dumps({"subject": "Report Ready"}),
+    )),
+)
